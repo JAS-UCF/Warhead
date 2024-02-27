193c52c0bbb1245a5d36256feb6545350629318c
BiggiePete <pter.cross222@gmail.com>
2024-02-26 23:59:09 -0500

    read bluetooth lines until failure

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,19 @@
+#include <Arduino.h>
+#include <BluetoothSerial.h>
 
+BluetoothSerial SerialBT;
 
 void setup()
 {
+  SerialBT.begin("LaunchKey", true);
+  Serial.begin(115200);
 }
-
 void loop()
 {
+  String inputText;
+  if (SerialBT.available())
+  {
+    inputText = SerialBT.readStringUntil('~');
+    Serial.println(inputText);
+  }
 }
