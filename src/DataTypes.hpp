2f9e2c8b54b76a54edd4823229765cd0fe7a9a26
BiggiePete <pter.cross222@gmail.com>
2024-02-27 00:09:30 -0500

    set up data types

---
+++ src/DataTypes.hpp
@@ -0,0 +1,23 @@
+#ifndef C5601963_DA06_46B2_8C3A_40EDBCE16198
+#define C5601963_DA06_46B2_8C3A_40EDBCE16198
+#include <Arduino.h>
+typedef struct
+{
+  float temperature;
+  String name;
+} temperature_sensor_t;
+typedef struct
+{
+  float pressure;
+  String name;
+} pressure_sensor_t;
+
+typedef struct
+{
+  temperature_sensor_t t1;
+  temperature_sensor_t t2;
+  temperature_sensor_t t3;
+  temperature_sensor_t t4;
+} sensor_data_t;
+
+#endif /* C5601963_DA06_46B2_8C3A_40EDBCE16198 */
--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,20 @@
 #include <Arduino.h>
 #include <BluetoothSerial.h>
+#include <DataTypes.hpp>
+#include <ArduinoJson.h>
 
 BluetoothSerial SerialBT;
-
+sensor_data_t sens_data;
 void setup()
 {
   SerialBT.begin("LaunchKey", true);
   Serial.begin(115200);
 }
+
 void loop()
 {
   String inputText;
+
   if (SerialBT.available())
   {
     inputText = SerialBT.readStringUntil('~');
