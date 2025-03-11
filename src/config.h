d75f5c6082b2020444b5ca1d8808c915ff646734
BiggiePete <pter.cross222@gmail.com>
2025-03-10 23:58:18 -0400

    thermocouples and aht have been confirmed working

--- src/config.h
+++ src/config.h
@@ -1,8 +1,8 @@
 #ifndef E569B5C8_5417_4856_8147_D935782B8565
 #define E569B5C8_5417_4856_8147_D935782B8565
 // Replace these with the datapoints from the RPI
-const char *ssid = "REPLACE_WITH_YOUR_SSID";
-const char *password = "REPLACE_WITH_YOUR_PASSWORD";
-const char *mqtt_server = "YOUR_MQTT_BROKER_IP_ADDRESS";
+const char *ssid = "BLAST_WIFI";
+const char *password = "BLAST1";
+const char *mqtt_server = "192.168.1.2";
 
 #endif /* E569B5C8_5417_4856_8147_D935782B8565 */
--- src/main.cpp
+++ src/main.cpp
@@ -17,6 +17,7 @@
 #include "config.h"
 #include <ArduinoJson.h>
 #include "max6675.h"
+#include <Adafruit_AHTX0.h>
 
 JsonDocument doc;
 
@@ -28,43 +29,86 @@ const int ledPin = 4;
 /*
 Essentially, if we want to read from several different spi devices (MAX6675) we need to bridge miso mosi, vin & gnd, the only thing we do that is new and different, is that we need a dedicated pin for CS (Chip select) we can set this to any pin, but need to set it high whenever we want to read from that device
 */
-// MAX6675 Thermocouple1();
+// first things first, we need to add thr 4 thermocouples, and get them working
+MAX6675 Thermocouple1(GPIO_NUM_14,GPIO_NUM_27,GPIO_NUM_12);
+MAX6675 Thermocouple2(GPIO_NUM_14,GPIO_NUM_26,GPIO_NUM_12);
+MAX6675 Thermocouple3(GPIO_NUM_14,GPIO_NUM_25,GPIO_NUM_12);
+MAX6675 Thermocouple4(GPIO_NUM_14,GPIO_NUM_32,GPIO_NUM_12);
+// ambient temperature and humidity
+Adafruit_AHTX0 aht;
 
 sensor_data_t sens_data;
 void setup()
 {
   Serial.begin(115200);
-  // set up mqtt
-  setup_wifi();
-  client.setServer(mqtt_server, 1883);
-  client.setCallback(callback);
+  delay(2000);
+  // once serial is available
+  aht.begin();
+  // // set up mqtt
+  // setup_wifi();
+  // client.setServer(mqtt_server, 1883);
+  // client.setCallback(callback);
 
-  pinMode(ledPin, OUTPUT);
+  // pinMode(ledPin, OUTPUT);
 }
 
 void loop()
 {
-  doc.clear();
 
-  // esp32 will read in the sensor data, then publish the data to mqtt
-  if (!client.connected())
-  {
-    reconnect();
-  }
-  client.loop();
+  // in this function, we should prepare all the data necessary, then prepare to send it to the mqtt server
+  sensors_event_t humidity, temp;
+  aht.getEvent(&humidity, &temp);
+  Serial.printf("t:%5f,h:%5f\n",temp.temperature,humidity.relative_humidity);
+  delay(500);
 
-  // read sensor data here
-  /*
-  Example :
-  doc["sensor1"] = sensor1_value;
-  doc["sensor2"] = sensor2_value;
-  doc["sensor3"] = sensor3_value;
-  doc["sensor4"] = sensor4_value;
-  */
+  // // esp32 will read in the sensor data, then publish the data to mqtt
+  // if (!client.connected())
+  // {
+  //   reconnect();
+  // }
+  // client.loop();
 
-  String mqtt_message_temp;
-  serializeJson(doc, mqtt_message_temp);
-  client.publish("esp32/sensor_data", mqtt_message_temp.c_str());
+  // // read sensor data here
+  // /*
+  // Example :
+  // doc["sensor1"] = sensor1_value;
+  // doc["sensor2"] = sensor2_value;
+  // doc["sensor3"] = sensor3_value;
+  // doc["sensor4"] = sensor4_value;
+  // */
+  // get thermocouple data
+  // JsonObject thermocouples = doc.createNestedObject("Thermocouples");
+  // thermocouples["tc1"] = Thermocouple1.readCelsius();
+  // thermocouples["tc2"] = Thermocouple2.readCelsius();
+  // thermocouples["tc3"] = Thermocouple3.readCelsius();
+  // thermocouples["tc4"] = Thermocouple4.readCelsius();
+
+
+  // get ambient air stuff
+  // sensors_event_t humidity, temp;
+  // aht.getEvent(&humidity, &temp);
+
+  // template for aht20
+  // JsonObject ambient = doc.createNestedObject("Ambient");
+  // ambient["temp"] = temp.temperature;
+  // ambient["humd"] = humidity.relative_humidity;
+
+// // template for water stats
+//   JsonObject water = doc.createNestedObject("Water");
+//   // Convert analog readings to appropriate units (adjust conversion as needed)
+//   water["pressure"] = analogReadToWaterPressure(analogRead(waterPressurePin));
+//   water["temperatureC"] = analogReadToWaterTemp(analogRead(waterTempPin));
+// // emplate for oil stats
+//   JsonObject oil = doc.createNestedObject("Oil");
+//   oil["pressure"] = analogReadToOilPressure(analogRead(oilPressurePin));
+//   oil["temperature"] = analogReadToOilTemp(analogRead(oilTempPin));
+
+// template for force stats
+  // doc["force"] = analogReadToForce(analogRead(forceRegulationPin));
+
+  // String mqtt_message_temp;
+  // serializeJson(doc, mqtt_message_temp);
+  // client.publish("esp32/sensor_data", mqtt_message_temp.c_str());
 }
 
 void setup_wifi()
